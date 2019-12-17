833791254d157ba0ceec2f79fff7965c6fa2e466
Shane Gable <gableshane@gmail.com>
2019-12-17 10:04:50 -0800

    server is live

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static('./public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', ( req , res) => {
-  res.render('index');
+app.get('/hello', ( req , res) => {
+  res.render('pages/index');
 });
 
 app.listen(PORT, () => {
