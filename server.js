78e6fb0149104f149e004fe49a9a21aa737ceedb
Shane Gable <gableshane@gmail.com>
2019-12-17 09:58:31 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ejs = require('ejs');
+const superagent = require('superagent');
+const PORT = process.env.PORT || 3000;
+
+const app =   express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', ( req , res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log('listening on', PORT);
+});
+
