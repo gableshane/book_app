a9021a0ef78fd778238f551615dd70af37297670
Shane Gable <gableshane@gmail.com>
2019-12-19 11:51:36 -0800

    callback functions and bug fixes

--- server.js
+++ server.js
@@ -19,8 +19,22 @@ const client = new pg.Client(`${DATABASE_URL}`);
 client.on('error', error => console.error(error));
 client.connect();
 
+const errorHandler = function (req , res) {
+  console.log(error);
+  res.render('pages/error');
+}
 
-app.get('/', ( req , res) => {
+// CALLBACK FUNCTION
+const queryById = function ( req , res ) {
+  let query = [req.params.id];
+  let SQL = 'SELECT * FROM books WHERE id=$1'
+  client.query(SQL, query).then( sql => {
+    let obj = sql.rows[0];
+    res.render('pages/detail', { oneBook : obj })
+  });
+}
+// CALLBACK FUNCTION
+const queryAll = function ( req, res ) {
   let SQL = 'SELECT * FROM books';
   const booksData = [];
   client.query(SQL).then( sqlData => {
@@ -31,37 +45,27 @@ app.get('/', ( req , res) => {
     }
     res.render('pages/index', { booksData : booksData, count : sqlData.rows.length });
   });
-});
-
-app.post('/search', ( req , res ) => {
+}
+// CALLBACK FUNCTION
+const apiAndStuff = function ( req , res ) {
   superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.searchType}+in${req.body.searchType}:${req.body.query}`).then( data => {
     books = [];
+    let isbn = null;
     for (let i = 0; i < 10; i++) {
       books.push(data.body.items[i]);
     }
     const results = books.map( book => {
-      let isbn = Object.values(book.volumeInfo.industryIdentifiers[0]).reduce( (a , b ) => a + ' ' + b);
+      if(book.volumeInfo.industryIdentifiers) {
+        isbn = Object.values(book.volumeInfo.industryIdentifiers[0]).reduce( (a , b ) => a + ' ' + b);
+      }
       return new Book(null, book.volumeInfo.imageLinks.smallThumbnail, book.volumeInfo.title, book.volumeInfo.authors, book.volumeInfo.description, isbn);
     });
     res.render('pages/searches/search', { results : results });
   })
-    .catch( error => {
-      console.log(error);
-      res.render('pages/error');
-      res.status(500).send(error.message);
-    })
-})
-
-app.get('/books/:id', ( req, res ) => {
-  let query = [req.params.id];
-  let SQL = 'SELECT * FROM books WHERE id=$1'
-  client.query(SQL, query).then( sql => {
-    let obj = sql.rows[0];
-    res.render('pages/detail', { oneBook : obj })
-  })
-});
-
-app.post('/books', (req , res ) => {
+    .catch(errorHandler)
+}
+// CALLBACK FUNCTION
+const insertDb = function (req , res ) {
   let SQL = 'INSERT INTO books ( author, title, isbn, image_url, description, bookshelf) VALUES ($1, $2, $3, $4, $5, $6)'
   let data = [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.description, req.body.bookshelf]
   client.query(SQL, data).then( sql => {
@@ -69,7 +73,12 @@ app.post('/books', (req , res ) => {
       res.redirect(`/books/${book.rows[0].id}`);
     });
   })
-});
+}
+
+app.get('/', queryAll);
+app.post('/search', apiAndStuff );
+app.get('/books/:id', queryById);
+app.post('/books', insertDb);
 
 // BOOK CONSTRUCTOR FUNCTION
 function Book( id=null, img_url='public/styles/images/placeholderbook.png', title='Title', author='Unknown', description='No description', isbn=null,bookshelf=null) {
@@ -81,7 +90,6 @@ function Book( id=null, img_url='public/styles/images/placeholderbook.png', titl
   this.isbn = isbn;
   this.bookshelf = bookshelf;
 }
-
 
 // SERVER LISTENS
 app.listen(PORT, () => {
